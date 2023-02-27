31f93c53d102fc64fb7e88710888ad505c8de976
AssylanAkhanuly <assylan.akhanuly@gmail.com>
2023-02-28 00:18:47 +0600

    lab6

---
+++ main.js
@@ -0,0 +1,20 @@
+// Task 4
+function Greeting(name) {
+    return "Hi, " + name + "!";
+}
+
+// Task 10 
+function AreEqual(number1, number2 ) {
+    if(number1 === number2) return true;
+    return false;
+}
+
+//Task 2
+function Perimeter(side1, side2) {
+    return side1*2+side2*2;
+}
+
+
+console.log(Greeting("Assylan"))
+console.log(AreEqual(1, 1));
+console.log(Perimeter(5, 6));
